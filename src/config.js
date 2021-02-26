333e062aede3ee1300f739fd48660be79aaa4baf
Steven <spang22@groton.org>
2021-02-25 21:47:02 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "stevens-notes-app-upload-2",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "ia6p6j7u53",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Q6vxExoNu",
+    APP_CLIENT_ID: "2d6lbtjbd17qaj959gm36996h3",
+    IDENTITY_POOL_ID: "us-east-1_Q6vxExoNu",
+  },
+};
+
+export default config;
